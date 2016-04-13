60c736bf93c27452bf4b8b4623a98bb97f7dff08
Judas Echeverria <judas1987@gmail.com>
2016-04-12 22:11:10 -0700

    Initialized repo

---
+++ server.js
@@ -0,0 +1,28 @@
+﻿var express = require("express");
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+    requireAuthentication : function (req, res, next) {
+        console.log("Private route hit");
+        next();
+    },
+    logger: function (req, res, next) {
+        console.log(req.method + " " + req.originalUrl + " at" + new Date().toString());
+        next();
+    }
+};
+
+//app.use(middleware.requireAuthentication);
+
+app.use(middleware.logger);
+
+app.get("/about/", middleware.requireAuthentication ,function (req, res) { 
+    res.send("About compadre!");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(PORT, function () { 
+    console.log("The web server has started on port " + PORT);
+});
