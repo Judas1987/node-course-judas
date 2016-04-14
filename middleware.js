4fffef39208056288af9a0547e80c85db7e86d1d
Judas Echeverria <judas1987@gmail.com>
2016-04-13 22:59:06 -0700

    Test

--- middleware.js
+++ middleware.js
@@ -4,7 +4,6 @@
         next();
     },
     logger: function (req, res, next) {
-        //console.log(req.method + " " + req.originalUrl + " at " + new Date().toString());
         console.log("Request: method - " + req.method + " to - " + req.originalUrl + " at" + new Date().toString());
         next();
     }
